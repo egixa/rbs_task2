1a62e23a649c1f31466a80faea811671110f80d2
egixa <irona114@yandex.ru>
2024-07-01 11:17:46 +0400

    Многопоточность в сортировке

--- task3.go
+++ task3.go
@@ -6,10 +6,57 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
-
+	"sync"
 	"time"
 )
 
+const asc = "asc"
+const desc = "desc"
+
+// Считывание флагов из строки терминала
+func flagParse() (string, string, error) {
+	rootFolder := flag.String("root", "", "Путь до директории для вывода структуры\n")
+	sortOption := flag.String("sort", asc, "Параметр сортировки:\n по убыванию -\n по возрастанию -")
+	flag.Parse()
+
+	// Проверка флагов на корректность ввода
+	if *rootFolder == "" {
+		fmt.Println(time.Now().Format("01-02-2006 15:04:05"), "Отсутствуют данные о местоположении директории.")
+		fmt.Println("Ожидаемые параметры вызова программы:")
+		flag.PrintDefaults()
+		return "", "", fmt.Errorf(fmt.Sprint("Отсутствуют данные о местоположении директории.\nОжидаемые параметры вызова программы:", rootFolder, sortOption))
+	}
+	if *sortOption != asc && *sortOption != desc {
+		*sortOption = asc
+		fmt.Println("Введен некорректный параметр сортировки. По умолчанию будет использована сортировка по возрастанию.")
+	}
+
+	// Проверка существования директории
+	_, err := os.Stat(*rootFolder)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", "", fmt.Errorf("Ошибка при обнаружении директории:", err)
+		}
+	}
+	return *rootFolder, *sortOption, nil
+}
+
+// Сортировка директории по входному параметру
+func sortDirectory(directoryContent []File, sortOption string) []File {
+	switch sortOption {
+	case desc:
+		sort.Slice(directoryContent, func(i, j int) (less bool) {
+			return directoryContent[i].Size > directoryContent[j].Size
+		})
+	case asc:
+		sort.Slice(directoryContent, func(i, j int) (less bool) {
+			return directoryContent[i].Size < directoryContent[j].Size
+		})
+	}
+	return directoryContent
+}
+
+// Форматирование размера файлов из байтов в килобайты, мегабайты и гигабайты
 func formatSize(size int64) string {
 	const gigabyte = 1000 * 1000 * 1000
 	const megabyte = 1000 * 1000
@@ -25,6 +72,7 @@ func formatSize(size int64) string {
 	return fmt.Sprintf("%d б", size)
 }
 
+// Создание массива информации о файлах
 type File struct {
 	Type string
 	Name string
@@ -33,6 +81,7 @@ type File struct {
 
 type Files []*File
 
+// Определение размера директории
 func dirSize(path string) (int64, error) {
 
 	var size int64
@@ -50,32 +99,15 @@ func dirSize(path string) (int64, error) {
 func main() {
 	start := time.Now()
 
-	rootFolder := flag.String("root", "", "Путь до директории для вывода структуры\n")
-	sortOptin := flag.String("sort", "asc", "Параметр сортировки:\n по убыванию -\n по возрастанию -")
-	flag.Parse()
-
-	if *rootFolder == "" {
-		fmt.Println(time.Now().Format("01-02-2006 15:04:05"), "Отсутствуют данные о местоположении директории.")
-		fmt.Println("Ожидаемые параметры вызова программы:")
-		flag.PrintDefaults()
+	//Считываем флаги
+	rootFolder, sortOption, err := flagParse()
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	if *sortOptin != "asc" && *sortOptin != "desc" {
-		*sortOptin = "asc"
-		fmt.Println("Введен некорректный параметр сортировки. По умолчанию будет использована сортировка по возрастанию.")
-	}
-
-	_, err := os.Stat(*rootFolder)
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Println("Ошибка при обнаружении директории:", err)
-			return
-		}
-	}
-
 	// Открываем директорию
-	dir, err := os.Open(*rootFolder)
+	dir, err := os.Open(rootFolder)
 	if err != nil {
 		fmt.Println("Ошибка при открытии директории", err)
 		return
@@ -89,42 +121,46 @@ func main() {
 		return
 	}
 
-	s := []File{}
-
-	// Выводим имена файлов и директорий
+	// Создаем массив структур с информацией о содержании директории
+	directoryContent := []File{}
+	var wg sync.WaitGroup
+	// Записываем имена, размеры файлов и директорий в массив структур
 	for _, file := range files {
 		filename := file.Name()
 		fileSize := file.Size()
-		filePath := filepath.Join(*rootFolder, filename)
+		filePath := filepath.Join(rootFolder, filename)
 
 		if file.IsDir() {
-			dirSize, err := dirSize(filePath)
-			if err != nil {
-				return
-			}
-			s = append(s, File{"Директория", filename, dirSize})
-			continue
+			wg.Add(1)
+
+			go func() {
+				defer wg.Done()
+
+				dirSize, err := dirSize(filePath)
+
+				if err != nil {
+					return
+				}
+
+				directoryContent = append(directoryContent, File{"Директория", filename, dirSize})
+			}()
+
 		} else {
-			s = append(s, File{"Файл", filename, fileSize})
+			directoryContent = append(directoryContent, File{"Файл", filename, fileSize})
 			continue
 		}
 	}
+	wg.Wait()
 
-	switch *sortOptin {
-	case "desc":
-		sort.Slice(s, func(i, j int) (less bool) {
-			return s[i].Size > s[j].Size
-		})
-	case "asc":
-		sort.Slice(s, func(i, j int) (less bool) {
-			return s[i].Size < s[j].Size
-		})
-	}
+	// Сортируем содержимое директории по указанному параметру
+	directoryContent = sortDirectory(directoryContent, sortOption)
 
-	for _, file := range s {
+	// Выводим содержимое в консоль
+	for _, file := range directoryContent {
 		fmt.Printf("%s %s Размер: %s\n", file.Type, file.Name, formatSize(file.Size))
 	}
 
+	// Время выполнения
 	duration := time.Since(start)
-	fmt.Println(duration)
+	fmt.Println("Программа завершена. Время выполнения:", duration)
 }
